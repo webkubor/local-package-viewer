ca8a31ac470fe1a6876cc4e1fb05eb811501c79e
webkubor <TWR-****-****-****-882E>
2024-12-24 12:31:02 +0800

    feat 配置样式,安装依赖,配置语法糖

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import PrimeVue from 'primevue/config';
 import Aura from '@primevue/themes/aura';
+import {klogger} from "kbor-logger";
+import '@/styles/index.scss'
+window.$logger = klogger();
+window.$logger?.success('当前环境', import.meta.env.MODE)
 
 createApp(App).use(PrimeVue, {
     theme: {
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,9 +2,23 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import {PrimeVueResolver} from '@primevue/auto-import-resolver';
 import Components from 'unplugin-vue-components/vite'
+import { fileURLToPath, URL } from 'url'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/styles/index.scss";`
+      }
+    }
+  },
   plugins: [vue(),
     Components({
       resolvers: [PrimeVueResolver()],
